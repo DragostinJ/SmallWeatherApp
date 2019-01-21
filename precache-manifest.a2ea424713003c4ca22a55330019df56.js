2b0e1056f60140d0f68a9e7bb066469859f46ae4
unknown <dragostinj@gmail.com>
2019-01-21 11:04:07 +0200

    Updates

---
+++ precache-manifest.a2ea424713003c4ca22a55330019df56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "0995bb2877c62a0837ee",
+    "url": "/seasons/static/js/main.0995bb28.chunk.js"
+  },
+  {
+    "revision": "229f9f0dddfa342cf5e8",
+    "url": "/seasons/static/js/1.229f9f0d.chunk.js"
+  },
+  {
+    "revision": "0995bb2877c62a0837ee",
+    "url": "/seasons/static/css/main.7fd9ce6a.chunk.css"
+  },
+  {
+    "revision": "b8e6cf90ee76960fcfe1dea8139414dc",
+    "url": "/seasons/index.html"
+  }
+];
